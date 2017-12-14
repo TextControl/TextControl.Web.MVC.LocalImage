c885b51a74989dcb1aa5e0600cc59dec14f4b859
Björn Meyer <bjoern@textcontrol.com>
2017-12-14 16:29:08 +0100

    Initial upload

--- README.md
+++
@@ -1,2 +0,0 @@
-# TextControl.Web.MVC.LocalImage
-This demo project shows how to add local images into TX Text Control.
---
+++ WebApplication59/Controllers/TXPrintController.vb
@@ -0,0 +1,19 @@
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Net
+Imports System.Net.Http
+Imports System.Web
+Imports System.Web.Http
+Imports TXTextControl.Web
+
+Namespace WebApplication59.Controllers
+	Public Class TXPrintController
+		Inherits ApiController
+		Public Function [Get]() As HttpResponseMessage
+			Dim printHandler = New TXPrintHandler()
+			printHandler.ProcessRequest(HttpContext.Current)
+			Return New HttpResponseMessage(HttpStatusCode.OK)
+		End Function
+	End Class
+End Namespace
